c9b1571a999326c534366d7f47a3cb32e248b4ee
wwicaksonoo <iamvitez@gmailcom>
2025-04-27 18:19:39 +0700

    update

--- lib/Tombol.dart
+++ lib/Tombol.dart
@@ -10,10 +10,6 @@ class Tombol extends StatelessWidget {
   Widget build(BuildContext context) {
     return ElevatedButton(
       onPressed: onPressed,
-      style: ElevatedButton.styleFrom(
-        backgroundColor: ganti ? Colors.white : Colors.black,
-        foregroundColor: ganti ? Colors.black : Colors.white,
-      ),
       child: Text(ganti ? 'mode terang' : 'mode gelap'),
     );
   }
